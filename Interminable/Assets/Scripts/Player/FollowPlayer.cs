f4ecc18242fbf757dc7a00b5e050b41bcdaad655
nvhuet <nvhuet@student.apc.edu.ph>
2022-05-06 13:32:00 +0800

    Section 3 Chapter 22

---
+++ Interminable/Assets/Scripts/Player/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerPos;
+    void Start()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        transform.position = playerPos.position;
+    }
+}
